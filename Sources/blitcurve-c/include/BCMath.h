549d10e14da7912b899769d6d974e430e21509df
Drew Crawford <drew@sealedabstract.com>
2020-06-03 17:54:19 -0500

    declare this static inline

--- Sources/blitcurve-c/include/BCMath.h
+++ Sources/blitcurve-c/include/BCMath.h
@@ -14,7 +14,7 @@ static inline bool BCIsNearlyEqual(bc_float_t a, bc_float_t b) {
 
 __attribute__((const))
 __attribute__((swift_name("IsNearlyEqual(a:b:)")))
-inline bool BCIsNearlyEqual2(bc_float2_t a, bc_float2_t b) {
+static inline bool BCIsNearlyEqual2(bc_float2_t a, bc_float2_t b) {
     bc_float2_t diff = a - b;
     
     return simd_norm_inf(diff) < 0.001;
