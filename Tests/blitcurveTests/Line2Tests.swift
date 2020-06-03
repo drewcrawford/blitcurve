d7acc343c8d4f66ef0fb1f5858658f06e5b4e1e3
Drew Crawford <drew@sealedabstract.com>
2020-06-03 02:14:15 -0500

    Add missing line2 tests

---
+++ Tests/blitcurveTests/Line2Tests.swift
@@ -0,0 +1,19 @@
+// Line2Tests.swift: Line2 tests
+// ©2020 DrewCrawfordApps LLC
+import XCTest
+@testable import blitcurve
+
+final class Line2Tests: XCTestCase {
+    func testLine() {
+        let l = Line2(point: .zero, angle: SIMD2<Float>(.pi/2,.pi/4), distance: SIMD2<Float>(12,13))
+        XCTAssert(l.a == .zero)
+        XCTAssert(l.b.lowHalf ~= SIMD2<Float>(0,12))
+        
+        let side = sqrtf(13*13/2)
+        XCTAssert(l.b.highHalf ~= BCFloat2(side, side));
+    }
+
+    static var allTests = [
+        ("testLine", testLine),
+    ]
+}
