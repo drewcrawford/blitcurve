cbf2c5c4583300c35f1dd9504442ebc5676b29b8
Drew Crawford <drew@sealedabstract.com>
2020-06-20 19:45:33 -0500

    Improve build script

--- makemetal.swift
+++ makemetal.swift
@@ -14,3 +14,24 @@ for file in try m.contentsOfDirectory(atPath: sourceDir) {
 }
 let outPath = (ProcessInfo.processInfo.environment["SRCROOT"] ?? ".") + "/blitcurve.metal"
 try! metalAccumulate.write(to: URL(fileURLWithPath:outPath), atomically: false, encoding:.utf8)
+
+func checkPbxProj() {
+    guard let srcRoot = ProcessInfo.processInfo.environment["SRCROOT"] else { return }
+    guard let projectName = ProcessInfo.processInfo.environment["PROJECT_NAME"] else { return }
+    guard let pbxproj = try? String(contentsOfFile: "\(srcRoot)/\(projectName).xcodeproj/project.pbxproj") else { return }
+    if !pbxproj.contains("blitcurve.metal in Sources") {
+        print("warning: Drag \(srcRoot)/blitcurve.metal into your xcodeproj")
+    }
+}
+
+func checkHeaderPath() {
+    guard let _ = ProcessInfo.processInfo.environment["XCODE_VERSION_MAJOR"] else { return }
+    let searchPaths = ProcessInfo.processInfo.environment["MTL_HEADER_SEARCH_PATHS"] ?? ""
+    if !searchPaths.contains("blitcurve-c/include") {
+        print("warning: Add ${HEADER_SEARCH_PATHS} to the 'Metal Compiler - Build Options - Header Search Paths' setting")
+    }
+    print("sp",searchPaths )
+}
+
+checkPbxProj()
+checkHeaderPath()
