064b1789c8e767922f9d0b9d0222fd3188ab023b
Drew Crawford <drew@sealedabstract.com>
2020-07-27 20:17:02 -0500

    [Rect] Playground for testing intersection algorithms
    Finds various counterexamples to the current implementation

--- Playgrounds/Rect.playground/Pages/Intersects.xcplaygroundpage/Contents.swift
+++ Playgrounds/Rect.playground/Pages/Intersects.xcplaygroundpage/Contents.swift
@@ -2,18 +2,66 @@
 import blitcurve
 import SwiftUI
 import PlaygroundSupport
+struct Inspector: View {
+    let label: () -> Text
 
-let r = Rect(center: SIMD2<Float>(10,10), lengths: SIMD2<Float>(1,1), angle: 0)
-let r2 = Rect(center: SIMD2<Float>(15,15), lengths: SIMD2<Float>(1,1), angle: 0)
+    let binding: Binding<Rect>
+    var body: some View {
+        GroupBox(label: label()) {
+            HStack {
+                Text("x")
+                Slider(value: binding.center.x, in:10...20)
+                
+                Text("\(binding.center.x.wrappedValue)")
+            }
+            HStack {
+                Text("y")
+                Slider(value: binding.center.y, in:10...20)
+                Text("\(binding.center.y.wrappedValue)")
+            }
+            HStack {
+                Text("angle")
+                Slider(value: binding.angle, in: 0...(.pi))
+                Text("\(binding.angle.wrappedValue)")
+
+            }
+            HStack {
+                Text("width")
+                Slider(value: binding.lengths.x, in:0...5)
+                Text("\(binding.lengths.x.wrappedValue)")
+            }
+            HStack {
+                Text("height")
+                Slider(value: binding.lengths.y, in: 0...5)
+                Text("\(binding.lengths.y.wrappedValue)")
+            }
+        }
+    }
+}
 
 struct R: View {
-    var body: some View {
-        BStack {
-            Rect.View(r)
-            Rect.View(r2)
+    @State var r1 = Rect(center: SIMD2<Float>(10,10), lengths: SIMD2<Float>(1,1), angle: 0)
+    @State var r2 = Rect(center: SIMD2<Float>(15,15), lengths: SIMD2<Float>(1,1), angle: 0)
+    var color: Color {
+        if Rect.intersects(r1.points4, r2.points4) {
+            return .red
         }
-        .scale(by: 30)
-        .frame(width: 300, height: 300, alignment: .topLeading)
+        return .black
+    }
+    var body: some View {
+        VStack {
+            BStack {
+                Rect.View(r1)
+                Rect.View(r2)
+            }
+            .scale(by: 30)
+            .foregroundColor(color)
+            .frame(width: 300, height: 200, alignment: .topLeading)
+            Inspector(label: {Text("r1")}, binding: $r1)
+            Inspector(label: {Text("r2")}, binding: $r2)
+
+        }
+        
     }
 }
 
