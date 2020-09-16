49914531f545a1248894a1d8a5ef69f318514889
Drew Crawford <drew@sealedabstract.com>
2020-09-15 19:31:48 -0500

    [AlignedCubic] Documentation

--- Sources/blitcurve-c/include/BCAlignedCubic.h
+++ Sources/blitcurve-c/include/BCAlignedCubic.h
@@ -8,10 +8,24 @@
 #include "BCMacros.h"
 /**Cubics can be "aligned" by applying translations and rotations that put them into a canonical form.  While we lose relative position about the cubic,
 we retain information intrinsic to the cubic.  In addition, certain operations are simplified.
+ @discussion There are various algorithms that can be implemented more quickly in the situation that the cubic is "aligned".  Aligning a cubic effectively creates a new coordinate space that cannot be naively compared to the global coordinate space.  However, the cubic still maintains its internal properties, like length and shape, and operations like that return the same result with less work.
  
- \discussion We align the cubic by
-    1. Setting it's 'a' coordinate to 0.  This eliminates the need to track a.
-    2. Rotating until the 'b' coordinate to be (bx,0).  This eliminates the need to track b.y.
+ This creates several opportunities for client optimizations.  Many cubics can map to a single aligned representation, which can be shared between them.  In addition, the cubic can be aligned once and the alignment can be re-used for many operations.  Exploiting this is however up to the client application.
+ 
+ In other cases, the alignment + algorithm may be the fastest implementation for a function, even if the alignment is not reused.
+ 
+Aligned cubics have the following properties:
+ 
+1. \c a is at 0,0 (cubic is translated).  For this reason, there is no \c a coordinate.
+ 
+2. \c b.y is 0 (cubic is rotated).  For this reason, \c b is represented only by  \c b_x.
+ 
+ In this situation, the evaluation function becomes
+ 
+ @code
+ b(x) = 3 * c * (1-t)^2 * t + 3 * d * (1-t) * t^2 + b * t^3
+ b(y) = 3 * c * (1-t)^2 * t + 3 * d * (1-t) * t^2
+ @endcode
  */
 __attribute__((swift_name("AlignedCubic")))
 typedef struct {
@@ -21,6 +35,10 @@ typedef struct {
     bc_float_t b_x;
 } BCAlignedCubic;
 
+/**  Aligns a cubic.
+ @see See the documentation for BCAlignedCubic for details on why you would do this.
+ @param c The cubic to align.
+ */
 __attribute__((const))
 __attribute__((swift_name("AlignedCubic.init(cubic:)")))
 BCAlignedCubic BCAlignedCubicMake(BCCubic c);
@@ -41,10 +59,19 @@ __attribute__((swift_name("AlignedCubic.__kappaPrime(self:t:)")))
 bc_float_t __BCAlignedCubicKappaPrime(BCAlignedCubic c, bc_float_t t);
 #endif
 
+/**This returns the bezier parameter \c (t) at which the maximum kappa can be found.
+ @param c the cubic
+ @param accuracy The maximum error allowed on \c t
+ @performance This implements a binary search.
+ */
 __attribute__((const))
 __attribute__((swift_name("AlignedCubic.maxKappaParameter(self:accuracy:)")))
 bc_float_t BCAlignedCubicMaxKappaParameter(BCAlignedCubic c, bc_float_t accuracy);
 
+/** This returns the curvature radius at a given bezier parameter.
+ @see This is the inverse of BCAlignedCubicKappa.
+ @param t The bezier parameter on interval [0,1] at which to measure the curvature.
+ */
 __attribute__((const))
 __attribute__((swift_name("AlignedCubic.curveRadius(self:t:)")))
 inline bc_float_t BCAlignedCubicCurveRadius(BCAlignedCubic c, bc_float_t t) {
