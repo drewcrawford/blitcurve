87661e4cb1d74f5b9ff476af619baa3d556a65b7
Drew Crawford <drew@sealedabstract.com>
2020-11-01 18:47:06 -0600

    [BCLut] Remove the LUT methods
    This is going to be implemented "elsewhere"

--- Sources/blitcurve-c/BCBezierParameter.c
+++ Sources/blitcurve-c/BCBezierParameter.c
@@ -6,14 +6,3 @@ extern inline bc_float_t BCVertexToBezierParameter(uint16_t vertexID, unsigned c
 
 __attribute__((overloadable))
 extern inline bc_float_t BCVertexToBezierParameter(uint16_t vertexID, unsigned char vertexesPerInstance, float lowerT, float upperT);
-
-__attribute__((overloadable))
-extern inline bc_float_t BCLUTParameterForIndex(uint8_t lutIndex, uint8_t lutCapacity, float lowerT, float upperT);
-__attribute__((overloadable))
-extern inline bc_float_t BCLUTParameterForIndex(uint8_t lutIndex, uint8_t lutCapacity);
-
-__attribute__((overloadable))
-extern inline uint8_t BCLUTIndexForParameter(float t, uint8_t lutCapacity, BCLUTRoundingMode roundingMode, float lowerT, float upperT);
-
-__attribute__((overloadable))
-extern inline uint8_t BCLUTIndexForParameter(float t, uint8_t lutCapacity, BCLUTRoundingMode roundingMode);
