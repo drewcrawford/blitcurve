68761b5e84992ed5c6feb095b9d4836a31f53275
Drew Crawford <drew@sealedabstract.com>
2020-11-15 18:28:21 -0600

    Selectively work around FB8897889=1 on affected GPUs.
    Basically, the workaround in the prior commit breaks apple silicon.
    
    See FB8897889 (AMD bug) and FB8899605 (apple bug).  For now it appears we need to run
    different code on each GPU.
    
    While we're waiting for a real fix I will compile this into simulator (only).
    We might want to apply it to macOS targets as well but I'm hoping this gets
    fixed before it becomes more of an issue.
    
    mt2-108

--- Sources/blitcurve-c/include/BCMacros.h
+++ Sources/blitcurve-c/include/BCMacros.h
@@ -4,6 +4,16 @@
 #ifndef BCMacros_h
 #define BCMacros_h
 
+//to work around FB8897889, we need __builtin_trap() for AMD.
+//However, the __builtin_trap causes FB8899605 on Apple Sillicon.
+//Allow support for conditionally compiling the workaround.
+//This is a pretty big hack and ought to be removed when we get real fixes.
+#ifdef FB8897889
+#define __BC_FB8897889 __builtin_trap();
+#else
+#define __BC_FB8897889
+#endif
+
 ///The BC_ASSERT macro will perform a platform-agnostic assertion check.
 ///BC_ASSERT will be a no-op if NDEBUG is defined.  This is set either by \c  Package.swift (CPU case) or in build settings (metal case)
 #ifdef NDEBUG
@@ -15,7 +25,7 @@
 #define __BC_ASSERT(X) assert(X)
 #else
 //note that on AMD, we require __builtin_trap following our assert pattern.  This seems to work around FB8897889.
-#define __BC_ASSERT(X) if (__builtin_expect(!(X),0)) {float device *f = 0; *f = 0; __builtin_trap();}
+#define __BC_ASSERT(X) if (__builtin_expect(!(X),0)) {float device *f = 0; *f = 0; __BC_FB8897889 }
 #endif
 #endif //NDEBUG
 
